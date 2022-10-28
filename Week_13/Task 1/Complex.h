aff885feabc9a3bd8b8591d800f1922622fa4280
anhbanlinhle <vietlinhleviet@gmail.com>
2022-10-29 04:57:32 +0700

    Week 13 Homework

---
+++ Week_13/Task 1/Complex.h
@@ -0,0 +1,14 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+class Complex {
+  private:
+    double real;
+    double ima;
+
+  public:
+    void print();
+    void getReal();
+    void getIma();
+};
+#endif
